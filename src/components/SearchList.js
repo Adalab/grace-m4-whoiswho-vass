2baa4835491d952a51ff8543ba42a135b1c10f7b
anamontiaga <amontiaga@gmail.com>
2019-10-02 17:56:48 +0200

    add scss searchlist

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -1,30 +1,30 @@
-import React from "react"
+import React from "react";
+import "../stylesheets/SearchList.scss";
 
 function printList(props) {
-    console.log("hola")
-    if (props.queryInput !== "") {
-        return props.allEmployees.filter(employee => employee.nombre_empleado.toUpperCase().includes(props.queryInput.toUpperCase()))
+  if (props.queryInput !== "") {
+    return props.allEmployees
+      .filter(employee => employee.nombre_empleado.toUpperCase().includes(props.queryInput.toUpperCase()))
 
-            .map(employee => {
-                return (
-                    <li data-id={employee.id_empleado} onClick={props.consolea}>{`${employee.nombre_empleado}` + ` ${employee.apellidos_empleado ? employee.apellidos_empleado : ""} `} </li>
-                );
-            })
-    }
-};
-
-const SearchList = (props) => {
-    console.log(props)
-    return (
-        <div>
-            <input type="text" onChange={props.getValue} value={props.queryInput}></input>
-            <ul> {printList(props)} </ul>
-        </div>
-
-    )
-
+      .map(employee => {
+        return (
+          <li className="employee__list--item" data-id={employee.id_empleado} onClick={props.consolea}>
+            {`${employee.nombre_empleado}` + ` ${employee.apellidos_empleado ? employee.apellidos_empleado : ""} `}{" "}
+          </li>
+        );
+      });
+  } else {
+  }
 }
 
+const SearchList = props => {
+  const classList = props.queryInput !== "" ? "employee__list" : "hidden";
+  return (
+    <div className="employee__searcher">
+      <input placeholder="Nombre del colaborador" className="employee__input" type="text" onChange={props.getValue} value={props.queryInput}></input>
+      <ul className={`${classList}`}> {printList(props)} </ul>
+    </div>
+  );
+};
 
-
-export default SearchList
+export default SearchList;
