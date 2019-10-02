1d1e74ec9e4d44a06f33ddb47dcb620c7486b7c8
Laura Ruiz <laura.ruizmazorra@gmail.com>
2019-10-02 10:26:06 +0200

    fix null and spaces problem and add shadow to selected

--- src/App.js
+++ src/App.js
@@ -17,50 +17,55 @@ class App extends React.Component {
     this.getData = this.getData.bind(this);
     this.getValue = this.getValue.bind(this);
     this.consolea = this.consolea.bind(this);
+    this.changeColorSelected = this.changeColorSelected.bind(this);
   }
 
   getData(id) {
-    fetch(`https://adalab-whoiswho.azurewebsites.net/api/employees/${id}`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        this.setState({
-          nombre_empleado: data.nombre_empleado + data.apellidos_empleado,
-          id: data.id_empleado
-        });
-        return fetch(`https://adalab-whoiswho.azurewebsites.net/api/employees/${data.id_superior}`)
-          .then(response => response.json())
-          .then(data => {
-            console.log(data);
-            this.setState({
-              parent: [
-                {
-                  nombre_empleado: data.nombre_empleado + data.apellidos_empleado,
-                  id: data.id_empleado,
-                  foto_empleado: foto
-                }
-              ]
-            });
-            if (data.id_superior !== "") {
-              return fetch(`https://adalab-whoiswho.azurewebsites.net/api/employees/${data.id_superior}`)
-                .then(response => response.json())
-                .then(data => {
-                  console.log(data);
-                  const spread = [
+    if (!isNaN(id)) {
+      fetch(`https://adalab-whoiswho.azurewebsites.net/api/employees/${id}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          this.setState({
+            nombre_empleado: `${data.nombre_empleado ? data.nombre_empleado : ""}` + ` ${data.apellidos_empleado ? data.apellidos_empleado : ""} `,
+            id: data.id_empleado
+          });
+          if (data.id_superior !== "") {
+            return fetch(`https://adalab-whoiswho.azurewebsites.net/api/employees/${data.id_superior}`)
+              .then(response => response.json())
+              .then(data => {
+                console.log(data);
+                this.setState({
+                  parent: [
                     {
-                      nombre_empleado: data.nombre_empleado + data.apellidos_empleado,
+                      nombre_empleado: `${data.nombre_empleado ? data.nombre_empleado : ""}` + ` ${data.apellidos_empleado ? data.apellidos_empleado : ""} `,
                       id: data.id_empleado,
                       foto_empleado: foto
-                    },
-                    ...this.state.parent
-                  ];
-                  this.setState({
-                    parent: spread
-                  });
+                    }
+                  ]
                 });
-            }
-          });
-      });
+                if (data.id_superior !== "") {
+                  return fetch(`https://adalab-whoiswho.azurewebsites.net/api/employees/${data.id_superior}`)
+                    .then(response => response.json())
+                    .then(data => {
+                      console.log(data);
+                      const spread = [
+                        {
+                          nombre_empleado: `${data.nombre_empleado ? data.nombre_empleado : ""}` + ` ${data.apellidos_empleado ? data.apellidos_empleado : ""} `,
+                          id: data.id_empleado,
+                          foto_empleado: foto
+                        },
+                        ...this.state.parent
+                      ];
+                      this.setState({
+                        parent: spread
+                      });
+                    });
+                }
+              });
+          }
+        });
+    }
   }
 
   getValue(ev) {
@@ -68,7 +73,12 @@ class App extends React.Component {
     this.getData(value);
   }
 
+  changeColorSelected(ev) {
+    const selected = ev.currentTarget
+    selected.classList.toggle("shadow")
+  }
   consolea(ev) {
+    this.changeColorSelected(ev)
     console.log(ev.currentTarget.dataset.id);
   }
 
@@ -77,7 +87,7 @@ class App extends React.Component {
     const MyNodeComponent = ({ node }) => {
       return (
         <div className="perfil">
-          <div className="initechNode">
+          <div className="initechNode" onClick={this.changeColorSelected}>
             <img src={node.foto_empleado} className="img" alt={node.nombre_empleado}></img>
           </div>
           <p className="name">{node.nombre_empleado}</p>
