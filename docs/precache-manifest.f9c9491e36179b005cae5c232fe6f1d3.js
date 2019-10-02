3ee199c372c3c8128b26623378252a055ace1a10
anamontiaga <amontiaga@gmail.com>
2019-10-02 09:40:15 +0200

    docs

---
+++ docs/precache-manifest.f9c9491e36179b005cae5c232fe6f1d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "624aed563b9c22385e4eadf6e46cf9d5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a60545af31162bc8fc9b",
+    "url": "./static/css/2.7a06f2be.chunk.css"
+  },
+  {
+    "revision": "c60d901d27da733db924",
+    "url": "./static/css/main.b1673201.chunk.css"
+  },
+  {
+    "revision": "a60545af31162bc8fc9b",
+    "url": "./static/js/2.509b6462.chunk.js"
+  },
+  {
+    "revision": "c60d901d27da733db924",
+    "url": "./static/js/main.e2da5db4.chunk.js"
+  },
+  {
+    "revision": "1f84b6134c1d62010abc",
+    "url": "./static/js/runtime-main.30b14073.js"
+  }
+]);
