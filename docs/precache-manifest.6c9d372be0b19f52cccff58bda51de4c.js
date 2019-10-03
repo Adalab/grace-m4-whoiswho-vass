c49a9443d310ea4e108b1bdcd418e20a33c6fac1
anamontiaga <amontiaga@gmail.com>
2019-10-03 12:51:34 +0200

    update docs

--- docs/precache-manifest.6c9d372be0b19f52cccff58bda51de4c.js
+++ docs/precache-manifest.6c9d372be0b19f52cccff58bda51de4c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3df76f5e23b9d8d8835d8a2f5a940172",
+    "revision": "83f7f7b54c4ca6277ed9a120f949e4bc",
     "url": "./index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.7a06f2be.chunk.css"
   },
   {
-    "revision": "3b1f4fe733264d568f0d",
-    "url": "./static/css/main.c3bad37a.chunk.css"
+    "revision": "9be1f89af2235b56249a",
+    "url": "./static/css/main.7aa7b39a.chunk.css"
   },
   {
     "revision": "a60545af31162bc8fc9b",
     "url": "./static/js/2.509b6462.chunk.js"
   },
   {
-    "revision": "3b1f4fe733264d568f0d",
-    "url": "./static/js/main.b5e2c975.chunk.js"
+    "revision": "9be1f89af2235b56249a",
+    "url": "./static/js/main.c9074c47.chunk.js"
   },
   {
     "revision": "1f84b6134c1d62010abc",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5513836d50bb4198240525ae8634baf8.js"
+  "./precache-manifest.6c9d372be0b19f52cccff58bda51de4c.js"
 );
 
 self.addEventListener('message', (event) => {
